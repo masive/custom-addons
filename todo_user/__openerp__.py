9793807877233b431bca4cc14ba1ad97c2c94852
Pavel Anchev <pavel.anchev@veriosoft.com>
2017-10-30 11:01:00 +0200

    Inherit todo_task

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -1,7 +1,4 @@
-{
-    'name': 'Multiuser To-Do',
-    'descriptio': 'Extend the To-Do app to multiuser.',
-    'author': 'Daniel Reis',
-    'depends': ['todo_app'],
-
-}
+{  'name': 'Multiuser To-Do',
+   'description': 'Extend the To-Do app to multiuser.',
+   'author': 'Daniel Reis',
+   'depends': ['todo_app'], }
