f542b171d9b28a616e6df4727cda06bb3326e525
Pavel Anchev <pavel.anchev@veriosoft.com>
2017-10-31 10:20:27 +0200

    Extending the record rules

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -2,5 +2,6 @@
    'description': 'Extend the To-Do app to multiuser.',
    'author': 'Daniel Reis',
    'depends': ['todo_app'],
-   'data': ['todo_user_view.xml',],
+   'data': ['todo_user_view.xml',
+            'security/todo_access_rules.xml',],
    }
