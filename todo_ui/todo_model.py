730dfa8cabb6ac536924219d031384be2a63efb6
Pavel Anchev <pavel.anchev@veriosoft.com>
2017-11-01 11:23:05 +0200

    Basic field types

--- todo_ui/todo_model.py
+++ todo_ui/todo_model.py
@@ -2,14 +2,30 @@
 
 from openerp import models, fields, api
 
+
 class Tag(models.Model):
     _name = 'todo.task.tag'
-    name = fields.Char('Name', 40, translate=True)
+    name = fields.Char('Name', size=40, translate=True)
+
 
 class Stage(models.Model):
     _name = 'todo.task.stage'
     _order = 'sequence, name'
-    _rec_name = 'name' # the default
-    _table = 'todo_task_stage' # the default
-    name = fields.Char('Name', 40, translate=True)
+    #String fields:
+    name = fields.Char('Name', size=40)
+    desc = fields.Text('Description')
+    state = fields.Selection(
+        [('draft', 'New'),
+         ('open', 'Started'),
+         ('done', 'Closed')], 'State')
+    docs = fields.Html('Documentation')
+    # Numeric fields:
     sequence = fields.Integer('Sequence')
+    perc_complete = fields.Float('% Complete', (3, 2))
+    # Date fields:
+    date_effective = fields.Date('Effective Date')
+    date_changed = fields.Datetime('Last Change')
+    # Other fields:
+    fold = fields.Boolean('Folded?')
+    image = fields.Binary('Image')
+
