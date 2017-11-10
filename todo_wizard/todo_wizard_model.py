ee3e4d7532c32e34cfc0ca9a986c2972c793ec63
Pavel Anchev <pavel.anchev@veriosoft.com>
2017-11-10 12:16:06 +0200

    Chapter 7 - Wizard business logic

--- todo_wizard/todo_wizard_model.py
+++ todo_wizard/todo_wizard_model.py
@@ -11,3 +11,22 @@ class TodoWizard(models.TransientModel):
     task_ids = fields.Many2many('todo.task', string="Tasks")
     new_deadline = fields.Date('Deadline to Set')
     new_user_id = fields.Many2one('res.users', string='Responsible to Set')
+
+    @api.multi
+    def do_mass_update(self):
+        self.ensure_one()
+        if not (self.new_deadline or self.new_user_id):
+            raise exceptions.ValidationError('No data to update')
+        # else:
+        _logger.debug('Mass update on Todo tasks %s', self.task_ids.ids)
+
+        if self.new_deadline: self.task_ids.write({'date_deadline': self.new_deadline})
+
+        if self.new_user_id: self.task_ids.write({'user_id': self.new_user_id.id})
+
+        return True
+
+    @api.onchange('new_user_id', 'new_deadline')
+    def debudding(self):
+        print "NEW USER ID: ", self.new_user_id.id
+        print "NEW DEADLINE: ", self.new_deadline
